247ae7f8d15afdd255e78571ac0c6cdb9d7cff47
Ne'matjonov Muhammadyusuf <nematjonov445@gmail.com>
2023-12-28 14:06:05 +0500

    Invoices yo'qligida chiqadigan page

--- src/pages/NoINvoicesPage.jsx
+++ src/pages/NoINvoicesPage.jsx
@@ -1,14 +1,16 @@
 import bgimage from "../assets/illustration-empty.svg";
-function NoINvoicesPage({isPending}) {
-    console.log(isPending);
+function NoINvoicesPage({ isPending }) {
+  console.log(isPending);
   return (
     <>
-    {!isPending && <div className="flex flex-col w-full max-w-[800px] mx-auto">
-      <img src={bgimage} alt="No Invoices" />
-      <h3 className="text-5xl text-center">No Invoices</h3>
-    </div>}
+      {!isPending && (
+        <div className="flex flex-col w-full max-w-[800px] mx-auto">
+          <img src={bgimage} alt="No Invoices" />
+          <h3 className="text-5xl text-center">No Invoices</h3>
+        </div>
+      )}
     </>
-  )
+  );
 }
 
 export default NoINvoicesPage;
